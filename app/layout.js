827e941aebff82c9a8a8b89798e370b04dd6a2cb
praveenraj <105029450+praveen-raj-r@users.noreply.github.com>
2024-09-30 06:42:05 +0530

    added app files

---
+++ app/layout.js
@@ -0,0 +1,33 @@
+import Header from "./_components/Header";
+import "@/app/_styles/globals.css";
+
+import { Josefin_Sans } from "next/font/google";
+import { ReservationProvider } from "./_components/ReservationContext";
+
+const josefin = Josefin_Sans({ subsets: ["latin"], display: "swap" });
+
+export const metadata = {
+  title: {
+    template: "%s / The Wild Oasis",
+    default: "Welcome / The Wild Oasis",
+  },
+  description:
+    "Luxurious cabin hotel, located in the heart of the Italian Dolomites, surrounded by beautiful mountains and dark forests.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${josefin.className} antialiased min-h-screen bg-primary-950 text-primary-100 flex flex-col relative`}
+      >
+        <Header />
+        <div className="grid flex-1 px-8 py-12">
+          <main className="w-full mx-auto max-w-7xl">
+            <ReservationProvider>{children}</ReservationProvider>
+          </main>
+        </div>
+      </body>
+    </html>
+  );
+}
---
+++ app/page.js
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import Link from "next/link";
+import bg from "@/public/bg.png";
+
+export default function Page() {
+  return (
+    <main className="mt-24">
+      <Image
+        placeholder="blur"
+        className="object-cover object-top"
+        fill
+        src={bg}
+        alt="Mountains and forests with two cabins"
+      />
+
+      <div className="relative z-10 text-center">
+        <h1 className="mb-10 font-normal tracking-tight text-8xl text-primary-50">
+          Welcome to paradise.
+        </h1>
+        <Link
+          href="/cabins"
+          className="px-8 py-6 text-lg font-semibold transition-all bg-accent-500 text-primary-800 hover:bg-accent-600"
+        >
+          Explore luxury cabins
+        </Link>
+      </div>
+    </main>
+  );
+}
