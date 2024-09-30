4eb9150cbdf4da286b630bcf4627e5af8ab2f469
praveenraj <105029450+praveen-raj-r@users.noreply.github.com>
2024-09-30 17:24:14 +0530

    Updated header flex container responsive

--- app/_components/Header.js
+++ app/_components/Header.js
@@ -4,7 +4,7 @@ import Logo from "@/app/_components/Logo";
 function Header() {
   return (
     <header className="px-8 py-5 border-b border-primary-900">
-      <div className="flex items-center justify-between mx-auto max-w-7xl">
+      <div className="flex flex-col items-center justify-between gap-5 mx-auto max-w-7xl md:flex-row">
         <Logo />
         <Navigation />
       </div>
