56213c418cdc9303112bbbe1119e1537fb9ffe7d
praveenraj <105029450+praveen-raj-r@users.noreply.github.com>
2024-09-30 17:25:23 +0530

    Aligned Header Logo

--- app/_components/Logo.js
+++ app/_components/Logo.js
@@ -6,13 +6,13 @@ function Logo() {
   return (
     <Link href="/" className="z-10 flex items-center gap-4">
       <Image
+        className="size-8 sm:size-10 md:size-auto"
         src={logo}
         height="60"
         width="60"
         quality={100}
         alt="The Wild Oasis logo"
       />
-      {/* <Image src="/logo.png" height="60" width="60" alt="The Wild Oasis logo" /> */}
       <span className="text-xl font-semibold text-primary-100">
         The Wild Oasis
       </span>
