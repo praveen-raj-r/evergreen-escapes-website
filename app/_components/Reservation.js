6234594c10e25d34b85a4292ddb6948c1ce00d7e
praveenraj <105029450+praveen-raj-r@users.noreply.github.com>
2024-10-01 16:44:54 +0530

    Update Reservation.js

--- app/_components/Reservation.js
+++ app/_components/Reservation.js
@@ -13,7 +13,7 @@ async function Reservation({ cabin }) {
   const session = await auth();
 
   return (
-    <div className="grid grid-cols-2 border border-primary-800 min-h-[400px]">
+    <div className="grid lg:grid-cols-2 border border-primary-800 lg:min-h-[400px] gap-10 lg:gap-0">
       <DateSelector
         settings={settings}
         bookedDates={bookedDates}
