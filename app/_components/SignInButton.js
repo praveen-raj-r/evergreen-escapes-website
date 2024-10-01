be9ebe2272bb376859154856a2a20259f9c60f85
praveenraj <105029450+praveen-raj-r@users.noreply.github.com>
2024-10-01 19:42:50 +0530

    Update SignInButton.js

--- app/_components/SignInButton.js
+++ app/_components/SignInButton.js
@@ -4,7 +4,7 @@ import { signInAction } from "../_lib/actions";
 function SignInButton() {
   return (
     <form action={signInAction}>
-      <button className="flex items-center gap-6 px-10 py-4 text-lg font-medium border border-primary-300">
+      <button className="flex items-center gap-6 px-4 py-4 font-medium border sm:px-10 sm:text-lg border-primary-300">
         <Image
           src="https://authjs.dev/img/providers/google.svg"
           alt="Google logo"
