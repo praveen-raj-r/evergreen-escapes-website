4f1fb4ff713853743fa05da538fd856eb24728a6
praveenraj <105029450+praveen-raj-r@users.noreply.github.com>
2024-10-01 19:43:52 +0530

    Update Header.js

--- app/_components/Header.js
+++ app/_components/Header.js
@@ -3,7 +3,7 @@ import Logo from "@/app/_components/Logo";
 
 function Header() {
   return (
-    <header className="px-8 py-5 border-b border-primary-900">
+    <header className="px-4 py-5 border-b sm:px-8 border-primary-900">
       <div className="flex flex-col items-center justify-between gap-10 mx-auto sm:gap-5 max-w-7xl md:flex-row">
         <Logo />
         <Navigation />
