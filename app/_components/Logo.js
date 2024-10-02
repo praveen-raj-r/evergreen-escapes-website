410436e59394c98994415c6bb996a716f026d141
praveenraj <105029450+praveen-raj-r@users.noreply.github.com>
2024-10-02 07:33:49 +0530

    Update Logo.js

--- app/_components/Logo.js
+++ app/_components/Logo.js
@@ -11,10 +11,10 @@ function Logo() {
         height="60"
         width="60"
         quality={100}
-        alt="The Wild Oasis logo"
+        alt="Evergreen Escapes logo"
       />
       <span className="text-xl font-semibold text-primary-100">
-        The Wild Oasis
+        Evergreen Escapes
       </span>
     </Link>
   );
